fe8fb136738455bc5d78f0481a45a4daad8a5d57
AnuAja <101627379+AnuAja@users.noreply.github.com>
2022-03-23 13:18:03 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Membuat objek Taxi
+            Taxi taxi1 = new Taxi();
+            Taxi taxi2 = new Taxi();
+            Taxi taxi3 = new Taxi();
+            Taxi taxi4 = new Taxi();
+            Taxi line = new Taxi();
+
+            // Pengesetan nilai properties
+            taxi1.DriverName = "Jono";
+            taxi1.OnDuty = true;
+            taxi1.NumPassenger = 10;
+
+            taxi2.DriverName = "Joni";
+            taxi2.OnDuty = true;
+            taxi2.NumPassenger = 6;
+
+            taxi3.DriverName = "Parno";
+            taxi3.OnDuty = true;
+            taxi3.NumPassenger = 12;
+
+            taxi4.DriverName = "Asep";
+            taxi4.OnDuty = false;
+            taxi4.NumPassenger = 0;
+
+            // Pemanggilan method
+            line.Line1();
+            taxi1.TaxiInfo();
+            taxi1.PickUpPassanger();
+            taxi1.DropOffPassanger();
+            line.Line2();
+
+            line.Line1();
+            taxi2.TaxiInfo();
+            taxi2.PickUpPassanger();
+            taxi2.DropOffPassanger();
+            line.Line2();
+
+            line.Line1();
+            taxi3.TaxiInfo();
+            taxi3.PickUpPassanger();
+            taxi3.DropOffPassanger();
+            line.Line2();
+
+            line.Line1();
+            taxi4.TaxiInfo();
+            taxi4.PickUpPassanger();
+            taxi4.DropOffPassanger();
+            line.Line2();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTaxi
+{
+    public class Taxi
+    {
+        // Properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public float NumPassenger { get; set; }
+
+        // Method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver : {0}", DriverName);
+            Console.WriteLine("On Duty : {0}", OnDuty);
+            Console.WriteLine("Num Passenger : {0}\n", NumPassenger);
+        }
+        public void PickUpPassanger()
+        {
+            if (NumPassenger == 0)
+            {
+                Console.WriteLine("{0} Tidak menjemput penumpang", DriverName);
+            }
+            else
+            {
+                Console.WriteLine("{0} Sedang menjemput penumpang", DriverName);
+            }
+        }
+        public void DropOffPassanger()
+        {
+            if (NumPassenger == 0)
+            {
+                Console.WriteLine("{0} Tidak mengantar penumpang", DriverName);
+            }
+            else
+            {
+                Console.WriteLine("{0} Selesai mengantar penumpang", DriverName);
+            }
+        }
+        public void Line1()
+        {
+            Console.WriteLine("================================");
+        }
+        public void Line2()
+        {
+            Console.WriteLine("================================\n\n\n");
+        }
+    }
+}
